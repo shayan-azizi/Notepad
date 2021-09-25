f7ff5796dc71e6cf972466ef55523eeb9b299ee2
Shayan Azizi <80700762+shayan-azizi@users.noreply.github.com>
2021-09-25 20:31:37 +0330

    Add Menu

--- main.py
+++ main.py
@@ -25,4 +25,29 @@ my_text.pack()
 # Configure Scrollbar
 text_scroll.config(command=my_text.yview())
 
+# Create Menubar
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+# Add Menu Tools
+# File Menu
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New File")
+file_menu.add_command(label="Open File")
+file_menu.add_command(label="Save")
+file_menu.add_separator()
+file_menu.add_command(label="Quit")
+
+# Edit Menu
+edit_menu = Menu(my_menu)
+my_menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Undo")
+edit_menu.add_command(label="Redo")
+edit_menu.add_separator()
+edit_menu.add_command(label="Cut")
+edit_menu.add_command(label="Copy")
+edit_menu.add_command(label="Cut")
+
+
 root.mainloop()
